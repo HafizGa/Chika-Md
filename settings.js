cb0ba8f19b35aaad06eead72a7b8f7247a21c0bb
HafizGa <142299462+HafizGa@users.noreply.github.com>
2023-08-15 15:30:08 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @AzharXD:)
+› Base Ori @Muhammad Saddam
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6282184088609'6282184088609,'6282184088609'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6282184088609'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6282184088609' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Hafiz Gaming:)' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Raja Bont' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Hafiz Gaming:)' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
+global.youtube = 'https://youtube.com/@Hafiz_Gaming01'
+global.ig = 'https://instagram.com/ahmadsurya356?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D'
 global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.myweb = 'https://s.id/Chika-bot'
+global.email = 'hafizbintang368@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Raja Bont' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Hafiz Gaming' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
